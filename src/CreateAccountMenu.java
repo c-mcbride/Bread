916eccac88ad5477b8db85ce07c1d615d444ca00
c-mcbride <califfmcbride@gmail.com>
2024-12-19 12:58:51 -0600

    Modify CreateAccountMenu to throw exceptions

--- src/CreateAccountMenu.java
+++ src/CreateAccountMenu.java
@@ -4,16 +4,38 @@ import java.util.Scanner;
 public class CreateAccountMenu {
     private Scanner scanner = new Scanner(System.in);
 
-    public BankAccount createAccount() {
-        System.out.println("Enter account holder's name:");
-        String name = scanner.nextLine();
+    public BankAccount createAccount() throws IllegalArgumentException{
+        String name = "";
+        double initialDeposit = -1;
 
-        System.out.println("Enter initial deposit amount:");
-        double initialDeposit = scanner.nextDouble();
+        //Get the account holders name
+        while (name.isEmpty()){
+            System.out.println("Enter account holder's name: ");
+            name = scanner.nextLine().trim();
+            if(name.isEmpty()){
+                throw new IllegalArgumentException("Name cannot be empty.");
+            }
+        }
 
-        BankAccount account = new BankAccount(name, initialDeposit);
-        System.out.println("Account created successfully!");
+        boolean validDeposit = false;
+        while(!validDeposit){
+            System.out.println("Enter initial deposit amount: ");
+            if(scanner.hasNextDouble()){
+                initialDeposit = scanner.nextDouble();
+                if(initialDeposit >= 0){
+                    validDeposit = true;
+                }
+                else{
+                    throw new IllegalArgumentException("Deposit amount cannot be negative");
+                }
+            }
+            else{
+                scanner.next();
+                throw new IllegalArgumentException("Invalid input. Please enter a numeric value");
+            }
+        }
 
-        return account;
+        //Return the newly constructed BankAccout object with valid parameters
+        return new BankAccount(name, initialDeposit);
     }
 }
